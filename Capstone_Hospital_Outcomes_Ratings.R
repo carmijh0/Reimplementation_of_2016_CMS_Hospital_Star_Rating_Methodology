107e32bf8aa68485d99dbd57018e80e6f6db98ca
James CarMichael <carmijh0@gmail.com>
2018-07-08 21:40:49 -0500

    preparing separate input hospital types

--- Capstone_Hospital_Outcomes_Ratings.R
+++ Capstone_Hospital_Outcomes_Ratings.R
@@ -3,7 +3,7 @@ library("dplyr")
 library("ggplot2")
 require(devtools);
 devtools::install_github("huangrh/rstarating");
-devtools::install_github("huangrh/relvm"); #!Problems installing custom package 'relvm' for lvm model recreation
+devtools::install_github("huangrh/relvm");
 devtools::install_github("huangrh/rclus");
 require(rstarating); require(relvm); require(rclus)
 
@@ -15,9 +15,8 @@ op <- out_dir("C:/Users/Carmijh0/Desktop/Data_Science/Capstone/data/R_ratings") 
 #2016 data
   
 x_og <- cms2016oct_input
-#View(x_og)
-x_og <- mstbl(x_og)
-fit_original <- relvm(x_og) # fit the non-adaptive lvm model
+x <- mstbl(x_og)
+fit_original <- relvm(x) # fit the non-adaptive lvm model
 sr <- rating(fit_original$groups$summary_score, iter.max = 1) 
 
 write.csv(fit_original$groups$pars, file=file.path(op,"Oct2016_par_truelvm_fit_original.csv")) #the parameters
@@ -31,9 +30,9 @@ write.csv(sr$summary_score, file=file.path(op,"Oct2016_sum_score_truelvm_fit_ori
 #2016 data
   
 x_1 <- cms2016oct_input
-x <- mstbl(x)
-fit_1 <- relm(x)
-sr_1 <- rating(fit2$groups$summary_score, method="kmeans", iter.max = 100)
+x <- mstbl(x_1)
+fit_1 <- relvm(x)
+sr_1 <- rating(fit_1$groups$summary_score, method="kmeans", iter.max = 100)
 
 write.csv(fit_1$groups$pars, file=file.path(op,"Oct2016_par_truelvm_fit_1.csv")) #the parameters
 write.csv(fit_1$groups$preds, file=file.path(op,"Oct2016_preds_truelvm_fit_1.csv")) #group scores
@@ -44,13 +43,44 @@ write.csv(sr$summary_score, file=file.path(op,"Oct2016_sum_score_truelvm_fit_1.c
 #Updated December 2017 data with corrected LVM and K means
 
 input <- rstarating::cms_star_rating_input_2017dec
-X <- mstbl(input)
+x <- mstbl(input)
 fit_2 <- relvm(x)
 sr_2 <- rating(x=fit_2$groups$summary_score,method="rclus2",score_col="sum_score",iter.max=5000)
 
 write.csv(fit_2$groups$pars, file=file.path(op,"Dec2017_par_truelvm_fit_2.csv")) #the parameters
 write.csv(fit_2$groups$preds, file=file.path(op,"Dec2017_preds_truelvm_fit_2.csv")) #group scores
 write.csv(sr_2$summary_score, file=file.path(op,"Dec2017_sum_score_truelvm_fit_2.csv")) #the summary scores & stars
+
+#Removing the measure weights in the LVM model to tackle the measure lodaing discrepancy. 
+
+input <- measure_manipulate(dat=cms_star_rating_input_2017dec, method="den_one", measures="hai_1")
+
+------------------------------------------------------------------
+  
+#Separting input files by hospital type 
+ach <- read.csv(file="../data/data/ach.csv", header=TRUE, sep=",")
+cah <- read.csv(file="../data/data/cah.csv", header=TRUE, sep=",")
+
+x_og$PROVIDER_ID <- gsub("[^0-9]", "", x_og$PROVIDER_ID) #cleaning id column
+x_og$PROVIDER_ID <- as.numeric(x_og$PROVIDER_ID)
+colnames(ach)[1] <- 'PROVIDER_ID'
+colnames(cah)[1] <- 'PROVIDER_ID'
+
+#Merging on provider ID for the two hospital types
+
+ach_1 <- inner_join(x_og, ach, by = 'PROVIDER_ID')
+cah_1 <- inner_join(x_og, cah, by = 'PROVIDER_ID')
+
+------------------------------------------------------------------
+
+#First attempt at applying the pipeline to the two separate inoput types
+#Will have to remove the hospital type column for mstbl cleaning function to work
+
+x <- mstbl(ach_1)
+fit_test <- relvm(x)
+sr_test <- rating(fit_test$groups$summary_score, method="kmeans", iter.max = 100)
+
+
 
 
 
